0048a8a088b80b1d517677fbf0af135dcef0856e
Becky Conning <becky.conning@icloud.com>
2015-11-19 14:47:56 +0000

    Added menu shortcut labels

--- wip/src/Dashboard/Menu/Component/State.purs
+++ wip/src/Dashboard/Menu/Component/State.purs
@@ -3,10 +3,10 @@ module Dashboard.Menu.Component.State where
 import Prelude
 
 import Data.Functor.Coproduct (Coproduct(..), left, right)
-
-import Halogen.Menu.Model as HalogenMenu
-import Halogen.Query (action)
+import Data.Maybe (Maybe(..))
 import Halogen.Menu.Component as HalogenMenu
+import Halogen.Menu.Component.State as HalogenMenu
+import Halogen.Query (action)
 
 import Model.CellType
 import Notebook.Component as Notebook
@@ -15,17 +15,20 @@ import Dashboard.Menu.Component.Query
 
 type StateP g = HalogenMenu.MenuP Value g
 
-initial :: HalogenMenu.Menu Value
-initial = HalogenMenu.makeMenu
+initial :: String -> HalogenMenu.Menu Value
+initial modifyerKeyString = HalogenMenu.makeMenu
   [ { label: "Notebook"
     , submenu:
         [ { label: "Rename/Move"
+          , shortcutLabel: Just $ modifyerKeyString ++ "⇧S"
           , value: notebookQueryToValue $ (Notebook.AddCell Explore) unit
           }
         , { label: "Delete"
+          , shortcutLabel: Nothing
           , value: notebookQueryToValue $ (Notebook.AddCell Explore) unit
           }
         , { label: "Publish"
+          , shortcutLabel: Just $ modifyerKeyString ++ "P"
           , value: notebookQueryToValue $ (Notebook.AddCell Explore) unit
           }
         ]
@@ -33,15 +36,19 @@ initial = HalogenMenu.makeMenu
   , { label: "Insert"
     , submenu:
         [ { label: "Query"
+          , shortcutLabel: Just $ modifyerKeyString ++ "1"
           , value: notebookQueryToValue $ (Notebook.AddCell Query) unit
           }
         , { label: "Markdown"
+          , shortcutLabel: Just $ modifyerKeyString ++ "2"
           , value: notebookQueryToValue $ (Notebook.AddCell Markdown) unit
           }
         , { label: "Explore"
+          , shortcutLabel: Just $ modifyerKeyString ++ "3"
           , value: notebookQueryToValue $ (Notebook.AddCell Explore) unit
           }
         , { label: "Search"
+          , shortcutLabel: Just $ modifyerKeyString ++ "4"
           , value: notebookQueryToValue $ (Notebook.AddCell Search) unit
           }
         ]
@@ -49,9 +56,11 @@ initial = HalogenMenu.makeMenu
   , { label: "Cell"
     , submenu:
         [ { label: "Evaluate"
+          , shortcutLabel: Just $ modifyerKeyString ++ "Enter"
           , value: notebookQueryToValue $ (Notebook.RunActiveCell) unit
           }
         , { label: "Delete"
+          , shortcutLabel: Nothing
           , value: notebookQueryToValue $ (Notebook.AddCell Explore) unit
           }
         ]
@@ -59,30 +68,39 @@ initial = HalogenMenu.makeMenu
   , { label: "Help"
     , submenu:
         [ { label: "Getting started"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/documentation/"
           }
         , { label: "Manual"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/documentation/front-end-manual/"
           }
         , { label: "SlamSQL reference"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/documentation/slamsql-reference/"
           }
         , { label: "SlamDown reference"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/documentation/slamdown-reference/"
           }
         , { label: "Cheatsheet"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/support/cheatsheet.pdf"
           }
         , { label: "How to guides"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/documentation/how-tos/"
           }
         , { label: "Securing access to SlamData"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "http://slamdata.com/documentation/quick-guide-resources/"
           }
         , { label: "Report a bug"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "mailto:support@slamdata.com?subject=Bug found"
           }
         , { label: "Request support"
+          , shortcutLabel: Nothing
           , value: helpURIToValue $ HelpURI "mailto:support@slamdata.com?subject=Request help"
           }
         ]
