935f0df3c01a52493e788bbcac3674ad77743326
Becky Conning <becky.conning@icloud.com>
2017-04-06 18:40:44 +0100

    Export LocalStorageF constructors

--- src/SlamData/LocalStorage.purs
+++ src/SlamData/LocalStorage.purs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-module SlamData.LocalStorage (Key(..), LocalStorageF, run) where
+module SlamData.LocalStorage (Key(..), LocalStorageF(..), run) where
 
 import SlamData.Prelude
 
